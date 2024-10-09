4d2ecce4e02a9766fe1a7fe77b0974ed35f80a29
Романов Игорь Владимирович <iv_romanov@betboom.org>
2024-10-09 18:55:57 +0300

    Добавил 3 задание 1 модуля

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = "Игорь"  # Создаем переменную name типа string
+print("Name:", name)  # Выводим значение переменной name
+
+age = 2023  # Создаем переменную age типа int
+print("Age:", age)  # Выводим значение переменной age
+
+age = age + 1  # Перезаписываем age, увеличивая его на 1
+print("New Age:", age)  # Выводим новое значение переменной age
+
+is_student = True  # Создаем переменную is_student типа bool
+print("Is Student:", is_student)  # Выводим значение переменной is_student
