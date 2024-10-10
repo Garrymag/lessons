5404507e87e127fc2790888865a76097b926d59d
Романов Игорь Владимирович <iv_romanov@betboom.org>
2024-10-10 11:11:02 +0300

    Добавил 1 задание 1 модуля

--- module_1_1.py
+++ module_1_1.py
@@ -2,8 +2,9 @@ completed_homework_count = 12  # Количество выполненных Д�
 spent_hours = 1.5  # Количество затраченных часов
 course_name = 'Python'  # Название курса
 average_time_per_task = spent_hours / completed_homework_count  # Время на одно задание
+
 # Вывод переменных на экран
-print("Всего задач:", completed_homework_count)
-print("Затрачено часов:", spent_hours)
-print("Курс:", course_name)
-print("Среднее время выполнения:", average_time_per_task, "часа.")
+print("Курс:", course_name,
+      ", всего задач:", completed_homework_count,
+      ", затрачено часов:", spent_hours,
+      ", среднее время выполнения", average_time_per_task, "часа.")
