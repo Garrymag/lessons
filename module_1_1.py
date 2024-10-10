c6c98127f645f4517217ccf2f76845e05bb0cb90
Романов Игорь Владимирович <iv_romanov@betboom.org>
2024-10-10 11:00:52 +0300

    Добавил 1 задание 1 модуля

---
+++ module_1_1.py
@@ -0,0 +1,9 @@
+completed_homework_count = 12  # Количество выполненных ДЗ
+spent_hours = 1.5  # Количество затраченных часов
+course_name = 'Python'  # Название курса
+average_time_per_task = spent_hours / completed_homework_count  # Время на одно задание
+# Вывод переменных на экран
+print("Всего задач:", completed_homework_count)
+print("Затрачено часов:", spent_hours)
+print("Курс:", course_name)
+print("Среднее время выполнения:", average_time_per_task, "часа.")
