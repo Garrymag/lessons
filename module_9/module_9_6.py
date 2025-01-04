caac2f209649d6cf34e6800aa91cb8f7f81bee63
Romanov <garrymag@yandex.ru>
2025-01-04 21:31:42 +0300

    Добавил задание 9_6

---
+++ module_9/module_9_6.py
@@ -0,0 +1,22 @@
+def all_variants(text):
+    # Перебираем все возможные длины подпоследовательностей
+    for length in range(1, len(text) + 1):
+        # Перебираем все возможные начальные позиции подпоследовательностей
+        for start in range(len(text) - length + 1):
+            # Извлекаем подпоследовательность заданной длины
+            subsequence = text[start:start + length]
+            # Возвращаем подпоследовательность с помощью yield
+            yield subsequence
+
+# Демонстрация работы функции
+def main():
+    # Создаем генератор для строки "abc"
+    a = all_variants("abcd")
+
+    # Итерируемся по всем подпоследовательностям
+    for i in a:
+        print(i)
+
+# Запуск основной функции
+if __name__ == "__main__":
+    main()
